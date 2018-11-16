636418ab1d68fd41887db65a9bde23ce3d7e1ee3
杨柳树下笑哈哈 <1367759585@qq.com>
2018-11-16 13:57:48 +0800

    init
    git commit -m init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-switch
+      v-model="value2"
+      active-color="#13ce66"
+      inactive-color="#ff4949">
+    </el-switch>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+        value1: true,
+        value2: true
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
